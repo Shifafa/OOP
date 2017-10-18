995a3510c44d222eec6d8fd08474d2219d400a4f
Shifafa <32517101+Shifafa@users.noreply.github.com>
2017-10-18 22:34:32 +0700

    Add files via upload

---
+++ tutorial.java
@@ -0,0 +1,16 @@
+public class tutorial {
+    public static void main (String[] args) {
+        System.out.println("Cara Memasak nasi");
+		System.out.println("1. Siapkan ricecooker sebagai alat memasak nasi");
+		System.out.println("2. siapkan beras sebanyak sesuai kebutuhann");
+		System.out.println("2. lalu masukan beras ke dalam wadah");
+		System.out.println("3. cuci beras tanpa menggunakan sabun");
+		System.out.println("4. lalu campurkan beras dengan air bersih ke dalam wadah yang telah di sediakan");
+		System.out.println("5. takar air setinggi satu ruas jari tangan");
+		System.out.println("6. masukan wadah yang berisi beras yang telah di campurkan air ke dalam ricecooker");
+		System.out.println("7. sambungkan ricecooker ke aliran listrik");
+		System.out.println("8. lalu tekan tombol cook untuk memulai memasak nasi");
+		System.out.println("9. tunggu nasi hingga tombol berubah menjadi warm");
+		System.out.println("10. nasi siap untuk dimakan");
+    }
+}
